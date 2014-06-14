fc711f1af0fc3d86a647182319473183143086cd
Rory Quinlan <rjamesquinlan@gmail.com>
2014-06-14 17:47:35 -0400

    initial design draft

---
+++ server.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require 'pry'
+
+
+
+def get_images
+  files = []
+  images = []
+  Dir.foreach("public/images") { |f| files << f}
+  files.each do |i|
+    if i.end_with?("jpg")
+      images << i
+    end
+  end
+  images.shuffle!
+end
+
+# binding.pry
+
+
+get '/' do
+  @images = get_images
+  erb :join
+end
